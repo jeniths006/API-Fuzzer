f2200fef432ddbdf8a8c8b47f00ce13ee87ebe6b
jeniths006 <jeniths006@gmail.com>
2026-07-28 20:17:24 +0100

    wip(response-analysis-engine): integrate vulnerability analysis pipeline

---
+++ src/main/java/com/example/API/Fuzzer/repository/AnalysisResultRepository.java
@@ -0,0 +1,14 @@
+package com.example.API.Fuzzer.repository;
+
+
+import com.example.API.Fuzzer.model.AnalysisResult;
+import com.example.API.Fuzzer.model.ExecutionResult;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface AnalysisResultRepository extends JpaRepository<AnalysisResult, Long> {
+
+    List<AnalysisResult> findByExecutionResult(ExecutionResult executionResult);
+
+}
